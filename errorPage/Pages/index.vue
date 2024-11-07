f678e42eaa8283b29436fe23184400442810ef63
GNAGNE Edva <gnagneloris@gmail.com>
2024-11-07 01:31:36 +0000

    premier commit de la page d'erreur 404

---
+++ errorPage/Pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="w-full h-screen flex-wrap p-6 flex justify-center gap-4 items-center">
+        <div class="rounded-full  p-4 bg-slate-50 flex justify-center items-center xl:w-[546px] xl:h-[546px] w-[273px] h-[273px] md:w-[364px] md:h-[364px]">
+            <NuxtImg class="w-[440px]" src="/OBJECTS.png" />
+        </div>
+        <div class="flex flex-col items-center sm:items-start gap-4 w-[329px] ">
+            <h3 class="font-bold text-6xl  sm:text-8xl">OUPS</h3>
+            <p class="font-inter sm:text-start text-center">Nous n'avons pas trouvé la page que vous recherchiez</p>
+            <button class="w-fit h-[44px] p-4 md:p-0 text-xs font-inter text-white bg-[#045B27] sm:w-[244px]">Retourner à l’accueil</button>
+        </div>
+    </div>
+  </template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
